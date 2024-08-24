d6a42a7e99cfdaa75d022bc14732057b39633dcb
oven8 <sambitsarkar2012@gmail.com>
2024-08-25 00:31:47 +0530

    updated modules for multi material interface and transfer matrices

--- disond/static/materials.py
+++ disond/static/materials.py
@@ -10,6 +10,7 @@ class materials:
     
     def mat_ref(self):
         mat = {
+            'Air' : 1.0,
             'SiO2' : np.sqrt(1+0.6961663/(1-(0.0684043/(self.lambda0*10**6))**2)+0.4079426/(1-(0.1162414/(self.lambda0*10**6))**2)+0.8974794/(1-(9.896161/(self.lambda0*10**6))**2)),
             'TiO2' : np.sqrt(5.913+0.2441/((self.lambda0*10**6)**2-0.0803))
         }
--- disond/transfermatrix.py
+++ disond/transfermatrix.py
@@ -2,16 +2,16 @@ import numpy as np
 import static as st
 
 class transfermatrix:
-    def __init__(self, n, omega, theta):
+    def __init__(self, n, d, omega, theta):
         self.n = n
-        self.omega = omega
-        self.theta = theta
+        self.d = d
+        self.omega = np.atleast_1d(omega)
+        self.theta = np.atleast_1d(theta)
         self.kx = self.kx()
         self.kz = self.kz()
-        self.kzratio = self.kzratio(dir=1)
-
+        
     def kx(self):
-        return np.outer(omega / st.C, np.sin(theta))
+        return np.outer(self.omega / st.C, np.sin(self.theta))
 
     def kz(self):
         n_broadcast = self.n[:, np.newaxis, np.newaxis]
@@ -20,18 +20,31 @@ class transfermatrix:
         return np.sqrt(((n_broadcast * omega_broadcast) / st.C)**2 - kx_broadcast**2)
 
     def kzratio(self, dir):
-        return self.kz[dir - 1, :, :] / self.kz[dir, :, :]
+        return self.kz[dir[1], :, :] / self.kz[dir[0], :, :]
 
-    def interface(self, kratio):
-        M1 = 0.5 * (1 + kratio)
-        M2 = 0.5 * (1 - kratio)
+    def interface(self, dir):
+        kzratio = self.kzratio(dir)
+        M1 = 0.5 * (1 + kzratio)
+        M2 = 0.5 * (1 - kzratio)
         return np.array([[M1, M2],[M2, M1]])
 
-    def propagation():
-        return 0
+    def propagation(self, mat_index):
+        exp = np.exp(-1j * self.kz[mat_index, :, :] * self.d[mat_index])
+        return np.array([[exp, np.zeros(exp.shape)], [np.zeros(exp.shape), 1/exp]])
 
-n = np.array([1.4,2.2])
-omega = np.array([1,2,3,4,5])
-theta = np.array([0.1,0.2,0.3,0.4,0.5])
-test = transfermatrix(n,omega,theta)
-print(test.kzratio.shape)
+class TM_materials(transfermatrix):
+
+    def __init__(self, mat_list, omega, theta):
+        materials = st.materials(mat_list)
+        n = materials.ref
+        d = materials.len
+        
+        super().__init__(n, d, omega, theta)
+
+#n = np.array([1.4,2.2])
+#omega = np.array([1,2,3,4,5])
+#theta = np.array([0.1,0.2,0.3,0.4,0.5])
+#test = transfermatrix(n,omega,theta)
+#print(test.kzratio.shape)
+TNM = TM_materials(["Air","SiO2","TiO2"],2,1)
+print(TNM.propagation(2).shape)
